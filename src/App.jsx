a00ae5a78e036c889c329d034d2b5e879e058c69
Tomi Halko <tomi.halko@gmail.com>
2023-08-28 10:32:26 +0300

    Tehtävä 2.6

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,39 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addName = (event) => {
+    event.preventDefault()
+    //console.log(newName)
+    //console.log(persons)
+    const newEntry = {
+      name: newName,
+    }
+    setPersons(persons.concat(newEntry))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    //console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
       <div>debug: {newName}</div>
     </div>
   )
