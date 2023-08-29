12c47b5fc8c6b767a0e75aa26d8eaa3e05a9f7c4
Tomi Halko <tomi.halko@gmail.com>
2023-08-29 13:14:34 +0300

    Rollataas takas staattiseen

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,34 @@ import { useState, useEffect } from 'react'
 import Persons from './components/Person'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
-import axios from 'axios'
+//import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  
+  //const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [originalPersons, setOriginalPersons] = useState(persons)
   const [filterInput, setFilterInput] = useState("")
 
+  /*
   const hook = () => {
     axios
     //.get("http://localhost:3001/persons")
     .get("https://raw.githubusercontent.com/tojutaha/puhelinluettelo/main/db.json")
     .then(response => {
-      setPersons(response.data)
+      console.log(response.data.persons)
+      setPersons(response.data.persons)
     })
   }
   useEffect(hook, [])
+  */
 
   const addPerson = (event) => {
     event.preventDefault()
